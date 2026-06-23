5b3d4e8297ea05afbde1b19483c78a07f307dec3
juanurielme-a11y <juanurielme@gmail.com>
2026-06-23 17:14:05 -0500

    Add cargarCSV function to handle CSV file uploads

---
+++ .github/workflows/cargarCSV.js
@@ -0,0 +1,23 @@
+window.cargarCSV = async () => {
+  const file = document.getElementById("csvFile").files[0];
+  const text = await file.text();
+
+  const lines = text.split("\n");
+
+  for (let i = 1; i < lines.length; i++) {
+    let data = lines[i].split(",");
+
+    const estudiante = {
+      Nombre: data[0],
+      Apellido: data[1],
+      Edad: parseInt(data[4]),
+      Semestre: parseInt(data[6])
+    };
+
+    const id = Date.now().toString();
+
+    await setDoc(doc(db, "Estudiantes", id), estudiante);
+  }
+
+  mostrar("✅ CSV cargado");
+};
