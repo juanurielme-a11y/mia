b98a10380ee3217385839b762deeb0d522f43cda
juanurielme-a11y <juanurielme@gmail.com>
2026-06-23 12:35:16 -0500

    Add prueba.java for GUI window example

---
+++ .github/workflows/prueba.java
@@ -0,0 +1,9 @@
+import javax.swing.*;import javax.swing.static void main(String[] args) {
+        JFrame ventana = new JFrame("Prueba");
+        ventana.setSize(300, 200);
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ventana.setVisible(true);
+    }
+}
+
+public class Test {
